4beb18d755a303807840558e5d25f51c27d7de57
Ionella <ionellapb27@gmail.com>
2021-12-01 23:07:20 +0300

    change script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+//1
+
+let user = {}
+    user.name = "John";
+    user.surname = "Smith";
+    user.name = "Pete";
+    delete user.name;
+
+//2
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+    }
+
+let sum = 0;
+for (let key in salaries){
+    sum += salaries[key];
+}
+console.log( "Suma salariilor:", sum)
+
+//3
+
+let obj ={
+    with: 200,
+    height: 800,
+    title: "My menu"
+
+}
+
+function multiplyNumeric(obj) {
+    for(let key in obj){
+           if(typeof obj[key]== 'number')  {
+            obj[key] *=2;
+        }  
+    }
+}
+
+multiplyNumeric(obj);
+
+console.log(obj)
+//4
+
+let user1={
+    name: "Ion",
+    age: 18
+}
+
+let user2={
+    name: "Ion",
+    age: 18
+}
+
+Object.assign(user2, {name: "Piotr", age: 37})
+console.log(user1)
+console.log(user2)
+
+
+//5
+
+let admin = {
+    name: "Person",
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+    phone: "112",
+    email: "email@domain.com"
+    },
+    address: "Moldova"      
+    }
