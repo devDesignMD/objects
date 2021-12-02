c665e09614ba092eb6daf5d619dd7ada9fa264e4
Ionella <ionellapb27@gmail.com>
2021-12-02 21:42:05 +0300

    delete script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //1
 
 let user = {}
-    user.name = "John";
-    user.surname = "Smith";
-    user.name = "Pete";
-    delete user.name;
+user.name = "John";
+user.surname = "Smith";
+user.name = "Pete";
+delete user.name;
 
 //2
 
@@ -12,17 +12,17 @@ let salaries = {
     John: 100,
     Ann: 160,
     Pete: 130
-    }
+}
 
 let sum = 0;
-for (let key in salaries){
+for (let key in salaries) {
     sum += salaries[key];
 }
-console.log( "Suma salariilor:", sum)
+console.log("Suma salariilor:", sum)
 
 //3
 
-let obj ={
+let obj = {
     with: 200,
     height: 800,
     title: "My menu"
@@ -30,10 +30,10 @@ let obj ={
 }
 
 function multiplyNumeric(obj) {
-    for(let key in obj){
-           if(typeof obj[key]== 'number')  {
-            obj[key] *=2;
-        }  
+    for (let key in obj) {
+        if (typeof obj[key] == 'number') {
+            obj[key] *= 2;
+        }
     }
 }
 
@@ -42,12 +42,12 @@ multiplyNumeric(obj);
 console.log(obj)
 //4
 
-let user1={
+let user1 = {
     name: "Ion",
     age: 18
 }
 
-let user2={
+let user2 = {
     name: "Ion",
     age: 18
 }
@@ -64,27 +64,27 @@ let admin = {
     age: 123,
     salary: 1542.33,
     contacts: {
-    phone: "112",
-    email: "email@domain.com"
+        phone: "112",
+        email: "email@domain.com"
     },
-    address: "Moldova"      
-    }
+    address: "Moldova"
+}
 
-    Object.assign(admin,{phone: "+37312345678`"} )
-    delete admin.address
-    console.log(admin)
+Object.assign(admin, {phone: "+37312345678`"})
+delete admin.address
+console.log(admin)
 
-    //6
+//6
 
-    let number =[1, 2, 3, 4, null, 4, 3, undefined]
+let number = [1, 2, 3, 4, null, 4, 3, undefined]
 
-    Array.prototype.clean = function(deleteValue) {
-        for (var i = 0; i < this.length; i++) {
-          if (this[i] == deleteValue) {         
+Array.prototype.clean = function (deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
             this.splice(i, 1);
             i--;
-          }
         }
-        return this;
-      };
+    }
+
     console.log(number)
+}
